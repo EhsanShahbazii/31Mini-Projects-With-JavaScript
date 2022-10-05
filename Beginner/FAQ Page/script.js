846ee2bcbbbc6fcbcad34a6bf8ae8ed64502a91c
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-10-05 16:28:21 +0330

    Update script.js
    update

--- Beginner/FAQ Page/script.js
+++ Beginner/FAQ Page/script.js
@@ -2,6 +2,7 @@ const $ = document;
 let img_btn = $.querySelectorAll("img");
 let items = $.querySelectorAll(".item");
 
+// open FAQ part
 img_btn.forEach((item) => {
   item.addEventListener("click", (e) => {
     item.classList.toggle("act_img");
