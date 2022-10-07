bc48698f30e4590e52d131e69aec1513c5678f7b
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-10-07 21:25:57 +0330

    add color change

---
+++ Beginner/Color Change/js/script.js
@@ -0,0 +1,12 @@
+﻿let redValue, greenValue, blueValue;
+
+setInterval(function () {
+  redValue = Math.floor(Math.random() * 255);
+  greenValue = Math.floor(Math.random() * 255);
+  blueValue = Math.floor(Math.random() * 255);
+
+  console.log("rgb(" + redValue + "," + greenValue + "," + blueValue + ");");
+
+  document.body.style.backgroundColor =
+    "rgb(" + redValue + "," + greenValue + "," + blueValue + ")";
+}, 2000);
