85a22c221a82e14c4c5bca30b08da52eeb01881e
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-10-07 21:26:48 +0330

    add book a ticket

---
+++ Beginner/Book A Ticket/js/script.js
@@ -0,0 +1,24 @@
+﻿let countrySelectBox = document.querySelector(".countrySelect");
+let citySelect = document.querySelector(".citySelect");
+
+let countriesData = {
+  Iran: ["Tabriz", "Tehran", "Shiraz", "Esfahan", "Mashhad"],
+  Turkey: ["Istanbul", "Ezmir", "Ankara", "Antaliya"],
+  US: ["Los Angles", "San Diego", "Chicago"],
+};
+
+countrySelectBox.addEventListener("change", () => {
+  if (countrySelectBox.value === "Please Select") {
+    citySelect.innerHTML = "";
+    citySelect.innerHTML += "<option>Select City</option>";
+  } else {
+    let mainCountryName = countrySelectBox.value; // Us
+    let mainCountryCities = countriesData[mainCountryName];
+
+    citySelect.innerHTML = "";
+
+    mainCountryCities.forEach(function (city) {
+      citySelect.innerHTML += "<option>" + city + "</option>";
+    });
+  }
+});
